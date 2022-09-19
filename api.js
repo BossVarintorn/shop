1f1f09c22f88d144902cbfbfd0ea638c733ed24e
Boss <105100286+BossVarintorn@users.noreply.github.com>
2022-09-19 20:21:48 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 //Url to Google Sheets
 //const url = 'https://docs.google.com/spreadsheets/d/[sheet ID]/gviz/tq?';
-const url = 'https://docs.google.com/spreadsheets/d/1qqcewCVPu3hTQTlgLBHbPWKJShlVnEiKu4ZdHLctPvc/gviz/tq?';
+const url = 'https://docs.google.com/spreadsheets/d/1mBsls2l-pzy0qWWmsUIIMdAn-6O57BWzHWzioiKmO50/gviz/tq?';
 const productsContainer = document.querySelector('.products-grid');
     fetch(url)
     .then(res => res.text())
